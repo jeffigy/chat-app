3a38615efffa3165776058c10b39fc0da327d45e
jeffigy <ejeffydev@gmail.com>
2024-11-10 13:54:22 +0800

    message routes and controllers

--- src/routes/messageRoute.ts
+++ src/routes/messageRoute.ts
@@ -1,4 +1,14 @@
-import express from "express";
-const messageRoutes = express.Router();
+import { Router } from "express";
+import protectRoute from "../middlewares/protectRoute";
+import {
+  getMessages,
+  getUsersForSidebar,
+  sendMessage,
+} from "../controllers/messageController";
+const messageRoutes = Router();
+
+messageRoutes.get("/conversations", protectRoute, getUsersForSidebar);
+messageRoutes.get("/:id", protectRoute, getMessages),
+  messageRoutes.post("/send/:id", protectRoute, sendMessage);
 
 export default messageRoutes;
