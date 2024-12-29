ad785d070b1d216bc2eb2b418566d37655ee4f7b
jeffigy <ejeffydev@gmail.com>
2024-12-30 01:21:46 +0800

    reac-query setup

---
+++ client/src/lib/ReactQueryProvider.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({});
+
+const ReactQueryProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+};
+
+export default ReactQueryProvider;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "@/index.css";
 import App from "@/App.tsx";
 import { BrowserRouter } from "react-router";
+import ReactQueryProvider from "./lib/ReactQueryProvider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ReactQueryProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ReactQueryProvider>
   </StrictMode>,
 );
