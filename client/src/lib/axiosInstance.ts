89a17953649fff2c97975171f06a3949ff6feb0f
jeffigy <ejeffydev@gmail.com>
2024-12-30 22:42:49 +0800

    refactor: Change the generic error response into the message server is sending

--- client/src/lib/axiosInstance.ts
+++ client/src/lib/axiosInstance.ts
@@ -6,4 +6,14 @@ const axiosInstance = axios.create({
   withCredentials: true,
 });
 
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response) {
+      error.message = error.response.data.message ?? error.message;
+    }
+    return Promise.reject(error);
+  },
+);
+
 export default axiosInstance;
